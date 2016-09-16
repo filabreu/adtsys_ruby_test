cbf18e6e82dee71a39bee2b9e267039b8190df87
Filipe Abreu <filabreu@gmail.com>
2016-09-16 10:18:56 -0300

    Add validations to Make and Model models. Validate uniqueness of Make name. Validate uniqueness of Model name, scoped by make_id. Add webmotors_make_id setter in Model model to set it's related Make by webmotors_id. Add belongs to Make relationship in Model model.

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,2 +1,3 @@
 class Make < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,9 @@
 class Model < ActiveRecord::Base
+  belongs_to :make
+
+  validates :name, uniqueness: { scope: :make_id }
+
+  def webmotors_make_id=(value)
+    self.make = Make.find_by(webmotors_id: value)
+  end
 end
